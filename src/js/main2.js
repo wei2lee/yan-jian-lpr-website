48d883c401db07e85782f94b4443023a472a1726
wei2lee <infradesign@Infradesigns-iMac4.local>
2014-11-05 20:02:22 +0800

    progress

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-//    window.requestAnimationFrame(function ( /* time */ time) {
-//        // time ~= +new Date // the unix time
-//        $(window).trigger('resize');
-//    });
+    //    window.requestAnimationFrame(function ( /* time */ time) {
+    //        // time ~= +new Date // the unix time
+    //        $(window).trigger('resize');
+    //    });
+    
     $(window).resize(onResize);
-
     function onResize() {
         var wh = $(window).height();
         $center = $('.center-container');
@@ -19,41 +19,64 @@ $(document).ready(function () {
         $sidebarright.css('min-height', h + 'px');
     }
 
-
-
-    $.address.init(function (event) {
-        processJqueryAddressEvent(event);
-    }).bind('change', function (event) {
-        processJqueryAddressEvent(event);
-    });
-
-    var lastEvent = null;
-
-    function processJqueryAddressEvent(event) {
-        if (lastEvent && lastEvent.value == event.value) return;
-
-        var url = '';
-        var pathnames = ['/home'];
-//        if (pathnames.indexOf(event.value) >= 0) {
-        if(true){
-            url = 'partials' + event.value + '.html';
-        } else {
-            url = 'partials/home.html';
-        }
-
-        lastEvent = event;
-        $.ajax(url).done(function (data) {
-            var $cnt = $('.center-container').children();
-            $cnt.fadeOut(100, function(){
-                 
-            });
-            $('.center-container').empty();
-            $(data).appendTo($('.center-container'));
-            $('a').each(function () {
-                $(this).toggleClass('selected', $(this).text() == event.value);
-            });
-            $(window).trigger('resize');
-        });
+    
+    function onContentUnload($content) {
+        
     }
+    function onContentLoad($content, url) {
+        
+    }
+    
+    (function () {
+        $.address.init(function (event) {
+            processJqueryAddressEvent(event);
+        }).bind('change', function (event) {
+            processJqueryAddressEvent(event);
+        });
+
+        var lastEvent = null;
+        var fetchingPage = 0;
+
+        function processJqueryAddressEvent(event) {
+            if (fetchingPage) return;
+            if (lastEvent && lastEvent.value == event.value) return;
+
+            var url = null;
+            var dir = 'partials';
+            var htmlext = '.html';
+            if (/^\/home/.test(event.value)) url = dir + '/home' + htmlext;
+            if (/^\/location/.test(event.value)) url = dir + '/location' + htmlext;
+            if (/^\/register/.test(event.value)) url = dir + '/register' + htmlext;
+            if (/^\/contact/.test(event.value)) url = dir + '/contact' + htmlext;
+            if (/^\/e-brochure/.test(event.value)) url = dir + '/e-brochure' + htmlext;
+            if (/^\/concept/.test(event.value)) url = dir + '/concept' + htmlext;
+            if (/^\/facilities/.test(event.value)) url = dir + '/facilities/phase1' + htmlext;
+            if (/^\/floorplan/.test(event.value)) url = dir + '/floorplan/phase1' + htmlext;
+            if (/^\/siteplan/.test(event.value)) url = dir + '/siteplan/phase1' + htmlext;
+            if (/^\/gallery/.test(event.value)) url = null;
+
+            if (!url) return;
+
+            lastEvent = event;
+            fetchingPage = 1;
+            $.ajax(url).done(function (data) {
+                var $cnt = $('.center-container').children();
+                $cnt.fadeOut(100, function () {
+                    
+                });
+                $('.center-container').empty();
+                $(data).appendTo($('.center-container'));
+                $('a').each(function () {
+                    $(this).toggleClass('selected', $(this).text() == event.value);
+                });
+                $(window).trigger('resize');
+            }).always(function () {
+                fetchingPage = 0;
+            });
+        }
+    })();
+
+
+    //post initial
     $(window).trigger('resize');
 });
